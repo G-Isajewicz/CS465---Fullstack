a70b7fe68904df739c2ef3b40f106d890aed933d
Gregory Isajewicz <gaisajew@gmail.com>
2024-05-22 10:48:37 -0400

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,5 +1,8 @@
-/* Get travel view */
 
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* Get travel view */
 const travel = (req, res) => {
     res.render('travel', {title: 'Travlr Getaways'});
 };
